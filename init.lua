f9c790f254f929db750458c320166932057c2459
Jerry <jerry31539@yahoo.com>
2022-09-19 19:33:20 +0900

    change mappings

--- init.lua
+++ init.lua
@@ -369,6 +369,13 @@ local config = {
       --   },
       -- },
     },
+    mappings = {
+      n = {
+        gi = function() require("telescope.builtin").lsp_implementations() end,
+        gr = function() require("telescope.builtin").lsp_references() end,
+        gd = function() require("telescope.builtin").lsp_definitions() end,
+      },
+    },
   },
 
   -- Diagnostics configuration (for vim.diagnostics.config({}))
