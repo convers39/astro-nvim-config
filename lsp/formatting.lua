89d3549a8ba78a727ded16a14623a33aa495d893
Jerry <jerry31539@yahoo.com>
2023-01-17 20:50:02 +0900

    disable sumneko_lua formatting which collides with stylua

--- lsp/formatting.lua
+++ lsp/formatting.lua
@@ -1,7 +1,7 @@
 return {
   format_on_save = true, -- enable or disable automatic formatting on save
-  timeout_ms = 1000, -- adjust the timeout_ms variable for formatting
-  -- disabled = { "sumneko_lua" },
+  timeout_ms = 2000, -- adjust the timeout_ms variable for formatting
+  disabled = { "sumneko_lua" },
   -- filter = function(client)
   -- only enable null-ls for javascript files
   -- if vim.bo.filetype == "javascript" then
