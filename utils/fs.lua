78bca0f7c8d9045541832e40598a225bafae5d98
Jerry <jerry31539@yahoo.com>
2023-03-18 12:03:57 +0900

    minor refactor

--- utils/fs.lua
+++ utils/fs.lua
@@ -1,18 +1,20 @@
 local M = {}
 
 function M.abs_path()
-  local path = vim.fn.expand "%:p" -- Get absolute path of current file
-  vim.fn.setreg("+", path) -- Set system clipboard register with path
+  local abs_path = vim.fn.expand "%:p" -- Get absolute path of current file
+  M.to_clipboard(abs_path)
 end
 
 function M.rel_path()
   local rel_path = vim.fn.fnamemodify(vim.fn.expand "%", ":.")
-  vim.fn.setreg("+", rel_path)
+  M.to_clipboard(rel_path)
 end
 
 function M.file_name()
   local name = vim.fn.expand "%:t"
-  vim.fn.setreg("+", name)
+  M.to_clipboard(name)
 end
+
+function M.to_clipboard(str) vim.fn.setreg("+", str) end
 
 return M
